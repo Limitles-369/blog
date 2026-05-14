bda6e37d3584dfb5c4d4b0934da826fd11dd214e
akash <akashsamui369@gmail.com>
2026-05-14 05:55:43 +0530

    fix: pin footer to bottom on short pages (tags, etc.) using flex column layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,7 +106,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="bg-[#F7F5F2] pl-[calc(100vw-100%)] text-black antialiased dark:bg-[#0f0f0f] dark:text-white">
+      <body className="flex min-h-screen flex-col bg-[#F7F5F2] pl-[calc(100vw-100%)] text-black antialiased dark:bg-[#0f0f0f] dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,7 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return <section className="mx-auto w-full px-4 sm:px-6 xl:px-10 2xl:px-16">{children}</section>
+  return (
+    <section className="mx-auto w-full flex-1 px-4 sm:px-6 xl:px-10 2xl:px-16">{children}</section>
+  )
 }
